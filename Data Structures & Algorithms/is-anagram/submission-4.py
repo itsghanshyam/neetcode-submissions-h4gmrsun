088f76b3a3617ab89a8c585d49e0fc9f315c88ef
Ghanshyam Matlane <pranavmatlane19@gmail.com>
2026-04-13 17:05:17 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+
+"""
+class Solution:
+
+
+
+    def isAnagram(self, s: str, t: str) -> bool:
+        dct1 = {\}
+        dct2 = {\}
+        for i in s:
+            dct1[i] = dct1.get(i,0) +1
+
+        for i in t:
+            dct2[i] = dct2.get(i,0) +1
+        return dct1 == dct2$0"""
